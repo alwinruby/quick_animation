e83c79916b9d21797a6ccb73c386141b3d80368f
alwinruby <alwinsolanky@gmail.com>
2021-09-06 05:21:25 +0100

    Animation with event handler

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
 window.addEventListener('DOMContentLoaded', function (e) {
-    document.querySelector('hp-presentation').onclick = handlePresentationClick;
-  });
-  
-  function handlePresentationClick(e) {
-    var current = document.querySelector('hp-slide.active');
-    var next = current.nextElementSibling;
-  
-    if (next) {
-      current.classList.remove('active');
-      next.classList.add('active');
-  
-      var aa = parseInt(next.getAttribute('data-autoadvance'));
-  
-      if (!isNaN(aa)) {
-        setTimeout(function (e) {
-          handlePresentationClick(e);
-        }, aa);
-      }
+  var presentation = document.querySelector('hp-presentation');
+
+  presentation.onclick = handlePresentationClick;
+  presentation.addEventListener('animationend', handleAnimationEnd, false);
+});
+
+function handlePresentationClick(e) {
+  var current = document.querySelector('hp-slide.active');
+  var next = current.nextElementSibling;
+
+  if (next) {
+    current.classList.remove('active');
+    next.classList.add('active');
+
+    var aa = parseInt(next.getAttribute('data-autoadvance'));
+
+    if (!isNaN(aa)) {
+      setTimeout(function (e) {
+        handlePresentationClick(e);
+      }, aa);
     }
   }
-  
+}
+
+function handleAnimationEnd(e) {
+  var slide = e.target.closest('hp-slide');
+  var aa = slide.getAttribute('data-autoadvance');
+
+  if (aa == 'animationend' && slide.classList.contains('active')) {
+    handlePresentationClick(e);
+  }
+}
