430e997f5be40e976f8a4a3264370a81001d4933
alwinruby <alwinsolanky@gmail.com>
2021-09-13 05:41:53 +0100

    fit and finish

--- js/main.js
+++ js/main.js
@@ -44,7 +44,9 @@ function handleAnimationEnd(e) {
 function setLearnImage(imageName) {
   var img = document.querySelector('hp-slide.active hp-learn img');
 
-  img.src = 'images/' + imageName + '.svg';
+  if (img){
+    img.src = 'images/' + imageName + '.svg';
+  }
 }
 
 var shapes = ['circle', 'diamond', 'square', 'triangle'];
